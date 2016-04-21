4db8833931637770f3654ec2dd89f091490fc006
Adrián de la Rosa <Adrian.delaRosa@lbigroup.es>
2016-04-21 13:33:30 +0200

    Add README. Change wget to quiet mode.

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-wget -O ~/.bash_profile https://raw.githubusercontent.com/adelarosab/dotfiles/master/.bash_profile
-wget -O ~/.gitconfig https://raw.githubusercontent.com/adelarosab/dotfiles/master/.gitconfig
-wget -O ~/.gitignore https://raw.githubusercontent.com/adelarosab/dotfiles/master/.gitignore
-wget -O ~/.git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+wget -q -O ~/.bash_profile https://raw.githubusercontent.com/adelarosab/dotfiles/master/.bash_profile
+wget -q -O ~/.gitconfig https://raw.githubusercontent.com/adelarosab/dotfiles/master/.gitconfig
+wget -q -O ~/.gitignore https://raw.githubusercontent.com/adelarosab/dotfiles/master/.gitignore
+wget -q -O ~/.git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
