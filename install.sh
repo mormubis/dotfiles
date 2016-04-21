783defe80878c54410960557b1b940ef9d837a68
Adrián de la Rosa <Adrian.delaRosa@lbigroup.es>
2016-04-21 13:28:56 +0200

    Add installer

---
+++ install.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+wget -O ~/.bash_profile https://raw.githubusercontent.com/adelarosab/dotfiles/master/.bash_profile
+wget -O ~/.gitconfig https://raw.githubusercontent.com/adelarosab/dotfiles/master/.gitconfig
+wget -O ~/.gitignore https://raw.githubusercontent.com/adelarosab/dotfiles/master/.gitignore
+wget -O ~/.git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
