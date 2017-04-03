df2f65e594cd1c13c40fceab1857844145cdc900
Adrián de la Rosa <adrian@delarosab.me>
2017-04-03 14:08:38 +0200

    ENH Replace wget by curl.
    FEA Add brew to setup
    FEA Add node to setup
    FEA Add java to setup
    FEA Add webstorm to setup
    FEA Add main npm dependencies to setup

--- install.sh
+++ install.sh
@@ -1,5 +1,24 @@
 #/bin/bash
-wget -q -O ~/.bash_profile https://raw.githubusercontent.com/adelarosab/dotfiles/master/.bash_profile
-wget -q -O ~/.gitconfig https://raw.githubusercontent.com/adelarosab/dotfiles/master/.gitconfig
-wget -q -O ~/.gitignore https://raw.githubusercontent.com/adelarosab/dotfiles/master/.gitignore
-wget -q -O ~/.git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+# config files
+curl "https://raw.githubusercontent.com/adelarosab/dotfiles/master/.bash_profile" -o ~/.bash_profile
+curl "https://raw.githubusercontent.com/adelarosab/dotfiles/master/.gitconfig" -o ~/.gitconfig
+curl "https://raw.githubusercontent.com/adelarosab/dotfiles/master/.gitignore" -o ~/.gitignore
+
+# brew
+! which brew && /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# node
+! which node && brew install node
+
+# java
+! which java && brew cask install java
+
+#webstorm
+! which webstorm && brew cask install webstorm
+
+# node dependencies
+npm i -g eslint eslint-config-defaults http-server npm-check-updates
+
+# completion files
+curl "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash" -o ~/.git-completion.bash
+npm completion > ~/.npm-completion.bash
