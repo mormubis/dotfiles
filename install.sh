46a9a4ab6b7e38be99d3034eff4e81fb96a562b0
Adrián de la Rosa <adrian@delarosab.me>
2018-09-18 11:37:16 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #/bin/bash
 # config files
 curl "https://raw.githubusercontent.com/adelarosab/dotfiles/master/.bash_profile" -o ~/.bash_profile
-curl "https://raw.githubusercontent.com/adelarosab/dotfiles/master/.eslint.json" -o ~/.eslint.json
 curl "https://raw.githubusercontent.com/adelarosab/dotfiles/master/.gitconfig" -o ~/.gitconfig
 curl "https://raw.githubusercontent.com/adelarosab/dotfiles/master/.gitignore" -o ~/.gitignore
 
 # brew
 ! which brew && /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# docker
+! brew install docker docker-compose docker-machine
 
 # git
 ! brew install git git-lfs
@@ -17,8 +19,14 @@ curl "https://raw.githubusercontent.com/adelarosab/dotfiles/master/.gitignore" -
 # node
 ! which node && brew install node
 
+# chrome
+! which java && brew cask install google-chrome homebrew/cask-versions/google-chrome-canary
+
 # java
 ! which java && brew cask install java
+
+# ngrock
+! which java && brew cask install ngrok
 
 # slack
 ! which slack && brew cask install slack
@@ -30,7 +38,7 @@ curl "https://raw.githubusercontent.com/adelarosab/dotfiles/master/.gitignore" -
 ! which webstorm && brew cask install webstorm
 
 # node dependencies
-npm i -g eslint eslint-config-defaults http-server prettier npm-check-updates
+npm i -g depcheck http-server prettier npm-check-updates
 
 # completion files
 curl "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash" -o ~/.git-completion.bash
