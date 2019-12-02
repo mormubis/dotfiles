d5ace7ee3bd7e4f767de601f4a90a1680ce4b37f
Adrian de la Rosa <adrian@delarosab.me>
2019-12-01 22:40:23 -0800

    Now using templates

--- .bash_profile
+++ .bash_profile
@@ -1,35 +1,8 @@
-# alias
-## default
-alias cd..='cd ..'
-alias deploy='ssh ubuntu@ec2-52-53-200-167.us-west-1.compute.amazonaws.com'
-alias git='hub'
-alias ll='ls -Galh'
-alias server='http-server -p 8000 1>/dev/null'
-alias simulator='open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app/'
-alias size='printf *%78s*\\n'
-alias workspace='cd ~/workspace'
-alias bye='clear; exit;'
 
-## git
-alias gb='git branch'
-alias gbr='git branch -r'
-alias gbs='git branch --set-upstream-to origin/`git symbolic-ref --short HEAD`'
-alias gc='git commit -a -m ""'
-alias gcm='git commit -m ""'
-alias gf='git fetch'
-alias gp='git pull --rebase'
-alias gpr='git pull-request -p -b development'
-alias gbp='git remote prune origin; git branch --merged development | grep -v development | xargs git branch -d'
-alias gs='git status -s'
-
-## npm
-alias npmls='npm ls --depth 0'
-alias npmup='npm-check -u'
-alias nrl='npm run lint'
-alias nrs='npm start'
-alias nrt='npm test'
-alias nrtw='npm test:watch'
-
+for file in ~/.{path,bash_prompt,exports,aliases,functions,extra}; do
+	[ -r "$file" ] && [ -f "$file" ] && source "$file";
+done;
+unset file;
 
 # completions
 ## git
@@ -41,23 +14,6 @@ fi
 if [ -f ~/.npm-completion.bash ]; then
   . ~/.npm-completion.bash
 fi
-
-# prompt
-## git branch
-function branch () {
-  git symbolic-ref --short HEAD &>/dev/null && echo '('`git symbolic-ref --short HEAD`')'
-}
-
-## current working directory
-function dir () {
-  p=${PWD/$HOME/\~}
-  p=${p/*workspace/+}
-  echo $p
-}
-# prompt
-PS1="\e[0;31m\u\e[m at \e[0;33m\h\e[m in \e[0;32m\$(dir)\$(branch)\e[m\n $ "
-SUDO_PS1="\e[0;31m\u\e[m at \e[0;33m\h\e[m in \e[0;32m\$(dir)\$(branch)\e[m\n # "
-
 
 # ssh-keys
 ssh-add -K ~/.ssh/github &>/dev/null
--- install.sh
+++ install.sh
@@ -1,39 +1,69 @@
-#/bin/bash
-# config files
-curl "https://raw.githubusercontent.com/adelarosab/dotfiles/master/.bash_profile" -o ~/.bash_profile
-curl "https://raw.githubusercontent.com/adelarosab/dotfiles/master/.gitconfig" -o ~/.gitconfig
-curl "https://raw.githubusercontent.com/adelarosab/dotfiles/master/.gitignore" -o ~/.gitignore
+#!/usr/bin/env bash
+
+echo "Hello $(whoami)! Let's get you set up."
+
+echo "installing homebrew"
 
 # brew
-! which brew && /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+echo "brew installing stuff"
+# Install more recent versions of some macOS tools.
+brew install grep openssh
+
+# Install font tools
+brew tap homebrew/cask-fonts
+brew cask install font-mononoki
+
+# bash completion
 # git
-! which git && brew install git git-lfs
-
 # hub
-! which hub && brew install hub
-
 # node
-! which node && brew install node
+# watchman
+brew install bash-completion git git-lfs  hub node watchman
 
+echo "installing apps with brew cask"
+
+# 1password
 # docker
-! which docker && brew cask install docker
-
+# dropbox
 # chrome
-! which google-chrome && brew cask install google-chrome
-
+# firefox
 # java
-! which java && brew cask install java
-
+# spotify
 # telegram
-! which telegram && brew cask install telegram
-
 # webstorm
-! which webstorm && brew cask install webstorm
+brew cask install 1password docker dropbox firefox google-chrome java spotify telegram webstorm
 
 # node dependencies
+# depcheck
+# eslint
+# gitmoji
+# http-server
+# nodemon
+# npm-check
+# npm-check-updates
+# prettier
 npm i -g depcheck eslint gitmoji-cli http-server nodemon npm-check npm-check-updates prettier
 
-# completion files
-curl "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash" -o ~/.git-completion.bash
-npm completion > ~/.npm-completion.bash
+echo "cloning dotfiles"
+
+# Retrieve config files
+curl "https://raw.githubusercontent.com/mormubis/dotfiles/master/.aliases" -o ~/.aliases
+curl "https://raw.githubusercontent.com/mormubis/dotfiles/master/.bash_profile" -o ~/.bash_profile
+curl "https://raw.githubusercontent.com/mormubis/dotfiles/master/.gitconfig" -o ~/.gitconfig
+curl "https://raw.githubusercontent.com/mormubis/dotfiles/master/.gitignore" -o ~/.gitignore
+curl "https://raw.githubusercontent.com/mormubis/dotfiles/master/.prompt" -o ~/.prompt
+
+echo "making system modifications:"
+curl https://raw.githubusercontent.com/mormubis/dotfiles/master/macos | bash
+
+echo "Generating an RSA token for GitHub"
+mkdir -p ~/.ssh
+touch ~/.ssh/config
+ssh-keygen -t rsa -b 4096 -C "adrian@delarosab.me"
+echo "Host *\n AddKeysToAgent yes\n UseKeychain yes\n IdentityFile ~/.ssh/id_rsa" | tee ~/.ssh/config
+eval "$(ssh-agent -s)"
+echo "run 'pbcopy < ~/.ssh/id_rsa.pub' and paste that into GitHub"
+
+echo "Done. Note that some of these changes require a logout/restart to take effect."
