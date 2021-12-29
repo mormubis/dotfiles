811ddae73be22449438b165e1360e6a4120d75f7
Adrian de la Rosa <adrian@delarosab.me>
2021-12-29 19:37:37 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -53,7 +53,7 @@ echo "customizing terminal"
 # install oh my zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 curl "https://raw.githubusercontent.com/mormubis/dotfiles/master/aliases.zsh" -o ~/.oh-my-zsh/custom/aliases.zsh
-sed -i='' 's/^plugins=.*$/plugins=(git nvm zsh-autosuggestions)g' ~/.zshrc
+sed -i='' 's/^plugins=.*$/plugins=(git nvm zsh-autosuggestions)/g' ~/.zshrc
 # powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 curl "https://raw.githubusercontent.com/mormubis/dotfiles/master/.p10k.zsh" -o ~/.p10k.zsh
